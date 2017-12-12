89f3bc925ae42cbc85fa4661c313420d4b7a160b
zhangyangzhou <907491547@qq.com>
2017-12-12 14:46:57 +0800

    天河项目

---
+++ js/main.js
@@ -0,0 +1,16 @@
+﻿$(function(){
+	$(".pos1").mouseover(function(){  
+		                $(this).prev("a").addClass("confous2");   
+		});
+		$(".pos1").mouseout(function(){  
+		                $(this).prev("a").removeClass("confous2");   
+		});
+	$(".menu li").each(function(index){
+			$(this).click(function(){
+				$(".menu li.tabFocus1").removeClass("tabFocus1");
+				$(this).addClass("tabFocus1");
+				$(".content li:eq("+ index +")").show().siblings().hide();
+		  	});
+
+		 });
+})
